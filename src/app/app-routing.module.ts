292dadb7931421813d2d15286351866311ef8237
Mark Maurer <Markmaurer1427@gmail.com>
2023-03-08 10:37:36 -0600

    Updated Routing Module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EventViewComponent } from './admin/event-view/event-view.component';
 import { HomeComponent } from './home/home.component';
+import { UserViewComponent } from './user/user-view/user-view.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'admin', component: EventViewComponent}
+  {path: 'admin', component: EventViewComponent},
+  {path: 'user', component: UserViewComponent}
 ];
 
 @NgModule({
